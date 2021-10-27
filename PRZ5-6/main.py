29a7237612f93aa8316dff7c76c77dfc4c7899c4
Alexander Samokhin <sshsmkhn@gmail.com>
2021-10-27 21:21:22 +0300

    add 3-4

--- PRZ5-6/main.py
+++ PRZ5-6/main.py
@@ -1,3 +1,4 @@
+#Zad1
 def kortsort(kortezh):
     check= True
     for i in kortezh:
@@ -9,4 +10,43 @@ def kortsort(kortezh):
     else:
         return kortezh
 
+#Zad2
+def kortrand(kortezh, searchNum):
+    kortNew = ()
+    check = False
+    for i in kortezh:
+        if (i == searchNum and check == False):
+            check = True
+            kortNew = kortNew+(i,)
+            continue
+        elif (i==searchNum and check == True):
+            kortNew = kortNew+(i,)
+            break
+
+        if (check == True):
+            kortNew = kortNew+(i,)
+    return kortNew
+
+#Zad3
+def listadd(newlist):
+    list = []
+    for i in newlist:
+        if i not in list:
+            list.append(i)
+    return sorted(tuple(list),reverse=True)
+
+#Zad4
+def kortEl(kortezh, element):
+    temp = 0
+    for i in kortezh:
+        if i == element:
+           return kortezh[0:temp]+kortezh[temp+1:kortezh.__sizeof__()]
+        temp+=1
+    return kortezh
+
+
+
 print(kortsort((1,4,2.2,3,5,6,1)))
+print(kortrand((1,4,8,3,4,6,1),4))
+print(listadd([2,3,3,2,5,7,4]))
+print(kortEl((1,4,2,3,5,6,3),4))
